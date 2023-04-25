1b7923c000ffba4af2ce27da757510da6a2d7997
Rocio Girardini <rogirardini@gmail.com>
2023-04-25 10:50:44 -0300

    Cambios en la estructura. Correcciones ciclo ID 2

--- reto-2/src/MainController.java
+++ reto-2/src/MainController.java
@@ -1,8 +1,6 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import serpapi.GoogleSearch;
-import serpapi.SerpApiSearchException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,8 +8,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainController {
     private MainView view;

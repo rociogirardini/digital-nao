ecf9e64742c3fbfcd187a8d0fdcaac5298a47eba
Rocio Girardini <rogirardini@gmail.com>
2023-06-07 21:39:37 -0300

    Ciclo ID 2 | Apps de consola 6 y 9

--- reto-4/miniproyecto-6/app.py
+++ reto-4/miniproyecto-6/app.py
@@ -1,33 +1,47 @@
-from flask import Flask, render_template, request
+UNITS = [
+    'cero', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve',
+    'diez', 'once', 'doce', 'trece', 'catorce', 'quince', 'dieciséis', 'diecisiete', 'dieciocho', 'diecinueve'
+]
 
-app = Flask(__name__)
+TENS = [
+    '', '', 'veinte', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa'
+]
 
-def convertir_a_palabras(numero):
-    unidades = ['', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve']
-    especiales = ['', 'once', 'doce', 'trece', 'catorce', 'quince', 'dieciséis', 'diecisiete', 'dieciocho', 'diecinueve']
-    decenas = ['', '', 'veinte', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa']
-    centenas = ['', 'ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos']
+HUNDREDS = [
+    '', 'ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos'
+]
 
-    if numero == 0:
-        return 'cero'
-    elif numero < 0 or numero > 999:
-        return 'Número fuera de rango'
-    elif numero <= 9:
-        return unidades[numero]
-    elif numero <= 19:
-        return especiales[numero - 10]
-    elif numero <= 99:
-        return decenas[numero // 10] + ' ' + convertir_a_palabras(numero % 10)
-    else:
-        return centenas[numero // 100] + ' ' + convertir_a_palabras(numero % 100)
+def numberToLetter(number):
+    if number < 20:
+        return UNITS[number]
+    
+    if number < 100:
+        unit = number % 10
+        ten = number // 10
+        
+        if unit == 0:
+            return TENS[ten]
+        else:
+            return f'{TENS[ten]} y {UNITS[unit]}'
+        
+    if number < 1000:
+        hundred = number // 100
+        rest = number % 100
+        
+        if rest == 0:
+            if hundred == 1:
+                return 'cien'
+            else:
+                return f'{HUNDREDS[hundred]}'
+        else:
+            return f'{HUNDREDS[hundred]} {numberToLetter(rest)}'
+    
+    raise ValueError('El número ingresado debe ser menor o igual a 999.')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        numero = int(request.form['numero'])
-        resultado = convertir_a_palabras(numero)
-        return render_template('index.html', resultado=resultado)
-    return render_template('index.html')
+number = int(input('Ingrese un número entre 0 y 999: '))
 
-if __name__ == '__main__':
-    app.run()
+if 0 <= number <= 999:
+    result = numberToLetter(number)
+    print(f'{number} en letras: {result}')
+else:
+    print('El número ingresado está fuera del rango permitido.')
--- reto-4/miniproyecto-9/app.py
+++ reto-4/miniproyecto-9/app.py
@@ -1,23 +1,21 @@
 import requests
-from flask import Flask, render_template, request
 
-app = Flask(__name__)
+API_KEY = "03c7ccfb8d2ce063eb5bfe9703f7f583"
 
-API_KEY = "03c7ccfb8d2ce063eb5bfe9703f7f583" 
+print("Bienvenido a la aplicación de búsqueda de películas.")
+busqueda = input("Ingrese el nombre de la película que desea buscar: ")
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        busqueda = request.form["busqueda"]
+url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={busqueda}"
+response = requests.get(url)
+data = response.json()
+peliculas = data["results"]
 
-        url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={busqueda}"
-        response = requests.get(url)
-        data = response.json()
-        peliculas = data["results"]
-    else:
-        peliculas = []
-
-    return render_template("index.html", peliculas=peliculas)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if peliculas:
+    for pelicula in peliculas:
+        titulo = pelicula["title"]
+        rating = pelicula["vote_average"]
+        print(f"Título: {titulo}")
+        print(f"Rating: {rating}")
+        print("---")
+else:
+    print("No se encontraron películas con ese nombre.")

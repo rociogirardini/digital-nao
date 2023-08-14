04f518ee33629f42980cb32c7203ea194bbb86ce
Rocio Girardini <rogirardini@gmail.com>
2023-08-14 20:51:56 -0300

    TypeORM | Ciclo 2

--- reto-7/src/app.module.ts
+++ reto-7/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksService } from './books/books.service';
 import { BooksController } from './books/books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController, BooksController], 
   providers: [AppService, BooksService], 
 })

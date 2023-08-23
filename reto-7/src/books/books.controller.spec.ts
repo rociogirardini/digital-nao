f82823972b829a9c4b51674e4dde643ace0f2c6f
Rocio Girardini <rogirardini@gmail.com>
2023-08-23 20:54:33 -0300

    Ciclo 3 | Archivos spec - test

--- reto-7/src/books/books.controller.spec.ts
+++ reto-7/src/books/books.controller.spec.ts
@@ -1,18 +1,115 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
+import { Book } from './book.class';
+import { Request } from 'express';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BookIdentity } from './book.entity';  // Asegúrate de importar la entidad correctamente
+import { BookDto } from './book.dto';
 
 describe('BooksController', () => {
   let controller: BooksController;
+  let service: BooksService;
+  let repository: Repository<BookIdentity>; // Asegúrate de tener la entidad correcta aquí
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
+      providers: [
+        BooksService,
+        {
+          provide: getRepositoryToken(BookIdentity), // Asegúrate de usar la entidad correcta
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
+    service = module.get<BooksService>(BooksService);
+    repository = module.get<Repository<BookIdentity>>(getRepositoryToken(BookIdentity));
     controller = module.get<BooksController>(BooksController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of books', async () => {
+      const result: Book[] = [new Book()]; 
+      
+      const mockRequest: Request = {
+        query: {},
+      } as Request;
+
+      jest.spyOn(service, 'findAll').mockResolvedValue(result); 
+      
+      const books = await controller.findAll(mockRequest);
+      
+      expect(books).toBe(result);
+    });
   });
+
+  describe('findBook', () => {
+    it('should return a book by ID', async () => {
+      const bookId = 'someBookId';
+      const result: Book = new Book();
+      jest.spyOn(service, 'findBook').mockResolvedValue(result);
+
+      const book = await controller.findBook(bookId);
+
+      expect(book).toBe(result);
+    });
+  });
+
+
+  describe('createBook', () => {
+    it('should create a new book', async () => {
+      const newBook: BookDto = {
+        title: '',
+        genre: '',
+        description: '',
+        author: '',
+        publisher: '',
+        pages: 0,
+        image_url: ''
+      };
+      const createdBook: Book = new Book();
+      jest.spyOn(service, 'createBook').mockResolvedValue(createdBook);
+
+      const book = await controller.createBook(newBook);
+
+      expect(book).toBe(createdBook);
+    });
+  });
+
+  describe('deleteBook', () => {
+    it('should delete a book by ID', async () => {
+      const bookId = 'someBookId';
+      const deletedBook: Book = new Book();
+      jest.spyOn(service, 'deleteBook').mockResolvedValue(deletedBook);
+
+      const book = await controller.deleteBook(bookId);
+
+      expect(book).toBe(deletedBook);
+    });
+  });
+
+  describe('updateBook', () => {
+    it('should update a book', async () => {
+      const bookId = 'someBookId';
+      const updatedBook: Book = new Book();
+      const newBookData: BookDto = {
+        title: '',
+        genre: '',
+        description: '',
+        author: '',
+        publisher: '',
+        pages: 0,
+        image_url: ''
+      };
+      jest.spyOn(service, 'updateBook').mockResolvedValue(updatedBook);
+
+      const book = await controller.updateBook(bookId, newBookData);
+
+      expect(book).toBe(updatedBook);
+    });
+  });
+
 });
--- reto-7/src/books/books.service.spec.ts
+++ reto-7/src/books/books.service.spec.ts
@@ -1,18 +1,91 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from './books.service';
+import { Book } from './book.class';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('BooksService', () => {
   let service: BooksService;
+  let repository: Repository<Book>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService],
+      providers: [
+        BooksService,
+        {
+          provide: getRepositoryToken(Book),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<BooksService>(BooksService);
+    repository = module.get<Repository<Book>>(getRepositoryToken(Book));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of books', async () => {
+      const result: Book[] = [new Book()];
+      jest.spyOn(repository, 'find').mockResolvedValue(result);
+
+      const books = await service.findAll({});
+
+      expect(books).toBe(result);
+    });
+  });
+
+  describe('findBook', () => {
+    it('should return a book by ID', async () => {
+      const bookId = '1';
+      const result: Book = new Book();
+      jest.spyOn(repository, 'findOne').mockResolvedValue(result);
+
+      const book = await service.findBook(bookId);
+
+      expect(book).toBe(result);
+    });
+  });
+
+  describe('createBook', () => {
+    it('should create a new book', async () => {
+      const newBook: Book = new Book();
+      jest.spyOn(repository, 'save').mockResolvedValue(newBook);
+
+      const createdBook = await service.createBook(newBook);
+
+      expect(createdBook).toBe(newBook);
+    });
+  });
+
+  describe('deleteBook', () => {
+    it('should delete a book by ID', async () => {
+      const bookId = '1';
+      const deleteResult = { affected: 1, raw: {} };
+      jest.spyOn(repository, 'delete').mockResolvedValue(deleteResult);
+
+      const deletionResult = await service.deleteBook(bookId);
+
+      expect(deletionResult).toEqual({});
+    });
+  });
+
+  describe('updateBook', () => {
+    it('should update a book', async () => {
+      const bookId = '1';
+      const newBook: Book = new Book();
+      const toUpdate: Book = new Book();
+      const updatedBook: Book = new Book();
+
+      jest.spyOn(repository, 'findOne').mockResolvedValue(toUpdate);
+      jest.spyOn(repository, 'save').mockResolvedValue(updatedBook);
+
+      const updated = await service.updateBook(bookId, newBook);
+
+      expect(updated).toBe(updatedBook);
+    });
   });
 });
